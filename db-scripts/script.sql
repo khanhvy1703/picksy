8d4522d475c31897125f8dd298cca85a81c8c495
khanhvy1703 <kvy.le@protonmail.com>
2025-06-08 23:06:44 -0400

    update

--- db-scripts/script.sql
+++ db-scripts/script.sql
@@ -4,5 +4,8 @@ show tables;
 select count(*) from movie;
 select movie_id, title from movie where title like '%Inception' order by movie_id asc;
 select count(*) from movie_genre;
-select * from movie_genre;
+select * from movie_genre order by movie_id desc;
+
+select count(*) from movie_language;
+select * from movie_language order by movie_id desc;
 
